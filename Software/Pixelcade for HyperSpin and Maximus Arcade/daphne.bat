edac32d790f4d974fce6ce39bd0c2906f08ecf71
Alfred Linke <alinke@yahoo.com>
2019-05-14 23:57:39 -0700

    daphne bat fixes

--- Software/Pixelcade for HyperSpin and Maximus Arcade/daphne.bat
+++ Software/Pixelcade for HyperSpin and Maximus Arcade/daphne.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal ENABLEDELAYEDEXPANSION
-F:\emulators\Daphne\JoyToKey_en\JoyToKey.exe
 set CONSOLE=daphne
 set GAMENAME=%2
 pixelcade.bat %CONSOLE% %GAMENAME%
+F:\emulators\Daphne\JoyToKey_en\JoyToKey.exe
+
